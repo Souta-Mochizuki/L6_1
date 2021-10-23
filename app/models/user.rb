0e1c7fec18d3a3706418aea7f6a2e69f7daf2c7e
Ubuntu <ubuntu@ip-172-31-21-56.ec2.internal>
2021-10-23 16:58:21 +0000

    validates 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_tweets, through: :likes, source: :tweet
+  validates :uid, uniqueness: true
   validates :password, presence: true, confirmation: true
   
   attr_accessor :password, :password_confirmation
